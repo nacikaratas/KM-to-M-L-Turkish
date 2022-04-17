ed1506f1f919ed935a2e76cf2ab24ef2850d7555
Naci Karataş <96789143+nacikaratas@users.noreply.github.com>
2022-04-17 12:36:10 +0300

    Add files via upload

---
+++ Kilometreyi Mile çevirme.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 12:49:55 2022
+
+@author: PC
+"""
+donusumorani=0.621371192
+km= float(input("Km yazınız="))
+mil=km*donusumorani
+print("Bu kadar km: "+str(km)+" Şu kadar Mil eder: "+str(mil))
+
+input("Çıkmak İçin herhangi bir tuşa basınız")
+
